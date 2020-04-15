0fd3208b052508b9b2177ee1263c9a14a675fb04
Kris2236 <63661068+Kris2236@users.noreply.github.com>
2020-04-15 10:18:46 +0200

    ADD faster LIS function to do

--- Lab_5(Algo_dynamiczne)/najdluzszy_podciag.py
+++ Lab_5(Algo_dynamiczne)/najdluzszy_podciag.py
@@ -1,4 +1,4 @@
-def LIS(A):
+def LIS_slow(A):                         # złożoność O(n^2)
     n = len(A)
     F = [1]*n
     for i in range(1,n):
@@ -7,6 +7,21 @@ def LIS(A):
                 F[i] = F[j]+1
     return max(F)
 
+#def LIS(A):                            # O( n*log(n) )
+    #vector<int> Warstwa[n];
+    #int Ostatni = 0, index;
+    #for (int i=0; i<n; i += 1)
+    #    wczytaj(A);
+    #    index = lower_bound(Warstwa.back, A);
+    #    //Czyli najmniejszy element większy od A (lub równy!).
+
+    #    Warstwa[index].Push_Back(A);
+    #    Ostatni = max(Ostatni, index); 
+    #//tutaj bierzemy maksimum ze wszystkich numerów
+    #//niepustych warstw
+    #wypisz(Ostatni);
+
+
 A = [1, 3, 2, 5, 4, 10]
 
-print(LIS(A))
+print(LIS_slow(A))
