b5c3cd6f227904cff3b2c2e4b92b37375fb8345f
Kris2236 <63661068+Kris2236@users.noreply.github.com>
2020-04-15 10:27:57 +0200

    Add print function

--- Lab_5(Algo_dynamiczne)/najdluzszy_podciag.py
+++ Lab_5(Algo_dynamiczne)/najdluzszy_podciag.py
@@ -1,11 +1,13 @@
-def LIS_slow(A):                         # złożoność O(n^2)
+def LIS_slow(A):    # złożoność O(n^2)
     n = len(A)
     F = [1]*n
+    P = [-1]*n      # parrent array
     for i in range(1,n):
         for j in range(i):
             if A[j] < A[i] and F[i] < F[j]+1:
                 F[i] = F[j]+1
-    return max(F)
+                P[i] = j    # wpisujemy indeks rodzica (poprzedniej wartosci)
+    return max(F), P
 
 #def LIS(A):                            # O( n*log(n) )
     #vector<int> Warstwa[n];
@@ -16,12 +18,17 @@ def LIS_slow(A):                         # złożoność O(n^2)
     #    //Czyli najmniejszy element większy od A (lub równy!).
 
     #    Warstwa[index].Push_Back(A);
-    #    Ostatni = max(Ostatni, index); 
+    #    Ostatni = max(Ostatni, index);
     #//tutaj bierzemy maksimum ze wszystkich numerów
     #//niepustych warstw
     #wypisz(Ostatni);
 
+def printLIS(A, P, i):
+    if P[i] >= 0:
+        printLIS(A, P, P[i])
+    print(A[i])
 
 A = [1, 3, 2, 5, 4, 10]
 
-print(LIS_slow(A))
+F, P = LIS_slow(A)
+printLIS(A, P, len(A)-1)
