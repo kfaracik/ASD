573fe770cc492256c93e3ba8dd7cd17346f8d6a4
Kris2236 <63661068+Kris2236@users.noreply.github.com>
2020-04-23 13:03:24 +0200

    upgrade comments

--- Sortowanie/bucketSort.py
+++ Sortowanie/bucketSort.py
@@ -1,14 +1,16 @@
+# Uzyteczny do sortowania liczb rzeczywistych
 # zał:
-#   - elementy są liczbami z zakresu [0.0  ...  1.0]
+#   - elementy są liczbami z zakresu [0.0  ...  1.0]    (mżna podzielić liczby aby były w tym zzakresie)
 #   - rozkład elementów w kubełkach jest równomierny
 #   - liczba kubełków jest liniowo zależna od liczby sortowanych elementów        (liczba kubełków = liczba el)   (k = len(tab)
 
-def bucketSort(t, k):   # k = liczba kubełków, względem których posortujemy tab
+def bucketSort(t, k):   # t - sortowana struktura;  k = liczba kubełków, względem których posortujemy tab
     b = [[] for i in range(k)]   #tworzymy k kubełków (list)
 
     for el in t:    b[int(k*el)].append(el) # rozdzielamy elementy do odpowiednich kubełków
 
     for i in range(k):  b[i].sort()     # sortujemy elementy w każdym kubełku
+    # prędkość algorytmu bucket sort silnie zależy od ilości elementów w kubełkach oraz metody sortowania
 
     res = []
     for i in range(k): res.extend(b[i]) # łączymy kubełki w jedną listę
