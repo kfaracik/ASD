3406551b2aa9bc7067cedd4bce32ea03a348e322
Kris2236 <63661068+Kris2236@users.noreply.github.com>
2020-05-11 17:45:42 +0200

    add print function

--- Lab_5(Algo_dynamiczne)/Knapsack.py
+++ Lab_5(Algo_dynamiczne)/Knapsack.py
@@ -1,3 +1,15 @@
+# 1. OPIS FUNKCJI: f(i,w) = największy zysk jaki można osiągnąć wybierająć spośród poierwszych i przedmiotów o indeksach od 0 do i, nie przekraczając zadanej wagi W
+# 2. ZAPIS REKURENCYJNY:
+                # Zał:
+                # f(0,w) = 0    , dla w<W[0]
+                #          P[0] , w > W[0]
+                # f(i,0) = 0
+                # w-W[i] >=0
+                #   f(i,w) = max( f(i-1,w) , f(i-1,w-W[i)+P[i] )
+# 3. IMPLENMEMNTACJA:
+
+# dobra złożoność dla relatywnie małych MaxW
+
 def knapsack(W, P, MaxW, M):
     n = len(W)
     F = [None]*n
@@ -6,7 +18,8 @@ def knapsack(W, P, MaxW, M):
 
     for w in range(W[0], MaxW + 1):
         F[0][w] = P[0]
-    M[0][W[0]] = 1              # set 1 row in mapa
+    M[0][W[0]] = 1              # ustraw mape
+    # ------     O(n*MaxW)   ------ złożoność do tego miejsca
 
     for i in range(1, n):
         for w in range(1, MaxW + 1):
@@ -15,10 +28,13 @@ def knapsack(W, P, MaxW, M):
                 F[i][w] = max(F[i][w], F[i-1][w-W[i]] + P[i])
 
             if F[i][w-1] != F[i][w]:
-                M[i][w] = 1     # set mapa
+                M[i][w] = 1     # ustaw mape
+    # ------     O(n*MaxW)   ------ złożoność do tego miejsca
 
-    printTab(F, n-1, MaxW)
-    printTab(M, n-1, MaxW)
+    printTab(F, n-1, MaxW)  # wypisuje wagi wyznaczonych przedmiotów - struktura knapsack
+    printTab(M, n-1, MaxW)  # wypisuje 1 jeśli przedmiot został wzięty, 0 wpp
+    printKnapsack(F, M, n-1, MaxW)
+
     return F[n-1][MaxW]
 
 def printTab(tab, w, k):
@@ -26,7 +42,15 @@ def printTab(tab, w, k):
         print(f'{tab[i]}')
     print()
 
-# tab z indeksami wziętych elementów
+# wypisuje wagi wyznaczonych przedmiotów
+def printKnapsack(F, M, w, MaxW):
+    for i in range(0, w):
+        for j in range(0, w):
+            if M[i][j]:
+                print(f'{F[i][j]}\t', end='')
+            else:
+                print(f'0\t', end='')
+        print()
 
 MaxW = 10
 W = [4, 1, 2, 4, 3, 5, 10, 3]
@@ -34,6 +58,6 @@ P = [7, 3, 2, 10, 4, 1, 7, 2]
 
 M = [None]*len(W)
 for i in range(len(W)):
-    M[i] = [0] * (MaxW + 1)  # Tworzymy tab dwuwymiarową
+    M[i] = [0] * (MaxW + 1)
 
 knapsack(W, P, MaxW, M)
