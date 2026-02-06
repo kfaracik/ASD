cbd9bdd0f9d11653832f3d7e6a7e9e3485838251
Krzysztof Faracik <63661068+kfaracik@users.noreply.github.com>
2026-02-06 18:46:00 +0100

    Implement quicksort algorithm with partitioning

---
+++ quicksort.ts
@@ -0,0 +1,27 @@
+function quickSort(
+  arr: number[],
+  low: number = 0,
+  high: number = arr.length - 1
+): number[] {
+  if (low < high) {
+    const pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+  return arr;
+}
+
+function partition(arr: number[], low: number, high: number): number {
+  const pivot = arr[high];
+  let i: number = low - 1;
+
+  for (let j = low; j < high; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+  return i + 1;
+}
